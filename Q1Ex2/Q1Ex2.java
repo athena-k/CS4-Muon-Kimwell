765ef1dbe5aa02f27fcbc67478756d84446163d4
athena-k <97677736+athena-k@users.noreply.github.com>
2022-09-13 22:08:05 +0800

    Add files via upload

--- Q1Ex2/Q1Ex2.java
+++ Q1Ex2/Q1Ex2.java
@@ -1,51 +1,29 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package Q1Ex2;
-
 import java.util.Scanner;
-
 /**
  *
- * @author MUON
+ * @author Athena Kimwell
  */
 public class Q1Ex2 {
-    /*String name;
-    int id, friendship;
-    double grade;
-    Q1Ex2 bff;
-    
-    public void Q1Ex1(){
-        System.out.printf("A new god has joined the server");
-    }
-    
-    this runs for every Q1Ex1, if u want to make student give name
-    public void Q1Ex1(String name){
-        this.name = name;
-        friendship = 0;
-    }
-            
-    public void introduce(){
-    System.out.printf("Hi im %s", name);
-    }*/
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         // TODO code application logic here
-        //class is identical to filename (new file)
-        //functions are attached to their classes
-        //call the function with the object in mind
-        //only 1 main method ie starting point
-        //tidies up the code (remember ex01 haha)
-        //can put an object inside an object (all students have bffs sanaol)
-        
+        //if you dont figure it out tomorrow just suck it up and ask skye :sob:
+        //coding is not good for my mental health bestie <33
+        //MJ NASAN KAAAA
+        String end = "false";
+        do {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Let's play Higher or Lower! What will you do?");
         System.out.println("- Start game");
+        System.out.println("- Change settings");
         System.out.println("- End application");
         
         int guesses = 3;
@@ -54,37 +32,47 @@ public class Q1Ex2 {
         String choice = sc.nextLine();
         //put a dowhile while playAgain is yes or choice is not end application 
         if(choice.equalsIgnoreCase("Start game")){
-            int random = (int) Math.floor(Math.random()*y) + 1;
+            int random = (int) Math.floor(Math.random()*y) + x;
             while(guesses != 0){
                 //System.out.println(random);
                 System.out.println("You have " + guesses + " guess(es) left. Guess a number between " + x + " and " + y);
-                int guess = sc.nextInt();
-                if(guess == random){
+                guesses--;
+                String guess = sc.nextLine();
+                int guess1 = Integer.parseInt(guess);
+                if(guess1 == random){
                     System.out.println("Congrats, you win!");
                     break;
                 }
-                else if(guess > random){
+                else if(guess1 > random){
                     System.out.print("Guess lower! ");
                 }
                 
-                else if(guess < random){
+                else if(guess1 < random){
                     System.out.print("Guess higher! ");
                 }
-                guesses--;
             }
             System.out.println("Game end! Play again (yes/no)?");
             String playAgain = sc.nextLine();
-            if (playAgain.equalsIgnoreCase("yes")){
-                
-            }
-            else if(playAgain.equalsIgnoreCase("no")){
-            
-            }
+                if (playAgain.equalsIgnoreCase("no")){
+                    System.out.println("Thanks for playing!");
+                    break;
+                }
+        }
+        else if (choice.equalsIgnoreCase("Change settings")){
+            System.out.println("What is the lower limit of the randomizer (default is 0)?");
+            x = sc.nextInt();
+            System.out.println("What is the upper limit of the randomizer (default is 10)?");
+            y = sc.nextInt();
+            System.out.println("How many guesses are allowed (default is 3)?");
+            guesses = sc.nextInt();
+            //di gumana?? hatdog
         }
         else if (choice.equalsIgnoreCase("End application")){
             System.out.println("Thanks for playing!");
+            break;
         }
-    }   
         
+        }while("false".equals(end));
+    }
     
 }
