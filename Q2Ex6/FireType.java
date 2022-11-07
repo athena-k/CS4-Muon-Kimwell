76e996d795d6926a5a3e841861008a197783a78e
athena-k <97677736+athena-k@users.noreply.github.com>
2022-11-07 08:57:17 +0800

    Add files via upload

---
+++ Q2Ex6/FireType.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q2Ex6;
+
+/**
+ *
+ * @author MUON
+ */
+public class FireType extends Monster{
+    public FireType(String n){
+        //super(insert unique traits here)
+    }
+}
---
+++ Q2Ex6/GrassType.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Q2Ex6;
+
+/**
+ *
+ * @author MUON
+ */
+public class GrassType extends Monster{
+    
+}
