a100220cea79976aa0b5df04fa743ed42b8fb376
athena-k <97677736+athena-k@users.noreply.github.com>
2023-03-09 08:59:12 +0800

    Add files via upload

---
+++ Q3Ex13/Q3Ex13.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXML.java to edit this template
+ */
+package Q3Ex13;
+
+import java.util.ArrayList;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Athena Kimwell
+ */
+public class Q3Ex13 extends Application {
+            
+    @Override
+    public void start(Stage stage) throws Exception {  
+        Subject math = new Subject("Math", "math.png", 4, 1.75);
+        Subject bio = new Subject("Biology", "biology.png", 3, 2.0);
+        Subject chem = new Subject("Chemistry", "chemistry.png", 3, 1.5);
+        Subject physics = new Subject("Physics", "physics.png", 3, 1.75);
+        Subject cs = new Subject("CS", "computer science.png", 1, 1.5);
+    
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
